721a9b1e41184ffdccc7c10a5f219139606b13b3
samya cardoso de david <dedavidsamya@gmail.com>
2021-10-14 15:45:01 +0200

    Adjusted final comments to code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,23 +4,6 @@ class  Counter extends Component {
     state = {
         value: this.props.value
     };
-    
-    // CONDITIONAL RENDERING: 
-    // renderTags() {
-        //I want to display one of the properties of my
-        //counter which is an array, and if the array is empty I display a message.
-        // As I can't use if/else in jsx, I have to create a method outside of the render and put my if/else logic here.
-        // and as I cannot use loops in .jsx, I'll display it
-        // through mapping each element of the array to a <li>.
-        // All elements of my array will be printed in separate lines as a list.
-        // I am adding a key to my <li> elements because React requires each element 
-        // of a list to be unique, so by giving the dynamic {tag} as a key, each <li>
-        // element has as a key the one element which it is being mapped to it.
-        // If the tags had ids, I could use {tag.id} to be more specific.
-        // if (this.state.tags.length === 0) return <p>'There are no tags.'</p>
-        
-        // return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>
-    // }
 
     // I am using an arrow function here because I want the handleincrement function
     // to inherit this and reference the current object as this.
@@ -31,38 +14,40 @@ class  Counter extends Component {
     };
 
     
- //props is a javascript object that contains all the properties
- //of a given object. In this case if I say this.props, I want to retrieve all the properties of the current object
+    //props is a javascript object that contains all the properties
+    //of a given object. In this case if I say this.props, I want to retrieve all the properties of the current object
     render() {
         console.log(this.props);
         //Here is what I want my render method to return. As I can only return 
         //one element, I wrap everything into a div. 
         return (
             <div>
-                {/* Inside the object props I can access children, which is the data I pass to a component inside its
-                closing tags. For example in my Counters element I inserted a "Counter number" as data inside the closing tags.
-                this data is accessible here in the children property */}
+            {/* Inside the object props I can access children, which is the data I pass to a component inside its
+            closing tags. For example in my Counters element I inserted a "Counter number" as data inside the closing tags.
+            this data is accessible here in the children property */}
               {this.props.children}
               <span className={this.getBtnClasses()}>{this.formatCount()}</span>
-              {/* Here I want the click on the button to trigger an event, incrementing the value of the counter
-              so I call the function handleIncrement which does exactly the incrementation of the 'value' property of 
-              my counter */}
+            {/* Here I want the click on the button to trigger an event, incrementing the value of the counter
+            so I call the function handleIncrement which does exactly the incrementation of the 'value' property of 
+            my counter */}
               <button 
               onClick={this.handleIncrement} 
               className="btn btn-secondary btn-sm m-2">
               Increment
               </button>
-{/* Here I am adding a button that triggers a Delete method on click. 
-The component that owns a piece of the state should be the one modifying it. In this particular case,
-the piece of the state that I want to delete is owned by the counters component, so Counters should be the component
-modifying the value (deleting it). To solve this problem and allow the state to be modified through the Counter component, it has to
-raise an event. onDelete (convention for naming events). So the Counter component raises the event, but the Counters component handles it 
-(therefore the method is implemented there, but passed to the Counter component via props). */}
+            {/* Here I am adding a button that triggers a Delete method on click. 
+            The component that owns a piece of the state should be the one modifying it. In this particular case,
+            the piece of the state that I want to delete is owned by the counters component, so Counters should be the component
+            modifying the value (deleting it). To solve this problem and allow the state to be modified through the Counter component, it has to
+            raise an event. onDelete (convention for naming events). So the Counter component raises the event, but the Counters component handles it 
+            (therefore the method is implemented there, but passed to the Counter component via props). */}
               <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m2">Delete</button>
             </div>
         );
     }
 
+    // For conditional rendering I have to defne methods with the if/else logic outside the render method and call them inside, when
+    //rendering. So here I define the methods which will modify the style of the buttons displayed for each circumstance (change in value)
     getBtnClasses() {
         let classes = "btn m-2 btn-sm btn-";
         classes += (this.state.value === 0) ? "warning" : "primary";
